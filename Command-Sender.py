5111a3d37b1b201727999dec6e19bbde2f250eb6
Alex <alexcousins78@gmail.com>
2017-09-06 22:21:38 +0100

    test

--- Command-Sender.py
+++ Command-Sender.py
@@ -3,7 +3,7 @@
 
 # command codes from http://www.aibohack.com/robosap/ir_codes.htm
 import robo
-Hex_integer == 0
+Hex_integer = 0
 
 rs=robo.Robo(21)	#create Robo object for GPIO 21
 #rs.send_code(0xB1)	#Issue reset command
@@ -12,7 +12,7 @@ while True:
 	Hex_string = raw_input("Input Command or bye to exit now  ")
 	if Hex_string == "bye" : break
 	print "Hex string is", Hex_string
-	Hex_integer == int(Hex_string, 16)
+	Hex_integer = int(Hex_string, 16)
 	print "Hex Integer is", Hex_string
 	rs.send_code(Hex_integer)
 	print "Command sent"
