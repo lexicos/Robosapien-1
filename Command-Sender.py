c12c78fa27526c9115131f0bf490b54a65842e7c
Alex <alexcousins78@gmail.com>
2017-09-06 07:18:00 +0100

    Added send commands

---
+++ Command-Sender.py
@@ -0,0 +1,19 @@
+# by Carl Monk (@ForToffee)
+# github.com/ForToffee/Robosapien
+
+# command codes from http://www.aibohack.com/robosap/ir_codes.htm
+import robo
+
+rs=robo.Robo(21)	#create Robo object for GPIO 21
+rs.send_code(0xB1)	#Issue reset command
+raw_input('Hit enter to get started')
+
+while True:
+	Hex_command = raw_input("Input Command or bye to exit")
+	if Hex_command == "bye" : break
+	print int(Hex_command, 16)
+	rs.send_code(Hex_command)
+	print "Command sent"
+	rs.send_code(0xB1)
+
+print "fin"
