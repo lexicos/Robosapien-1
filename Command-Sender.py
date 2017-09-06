fa2a7cff3e6dad616caf2538b1398b16b4fe0440
Alex <alexcousins78@gmail.com>
2017-09-06 21:57:09 +0100

    test

--- Command-Sender.py
+++ Command-Sender.py
@@ -6,12 +6,11 @@ import robo
 
 rs=robo.Robo(21)	#create Robo object for GPIO 21
 #rs.send_code(0xB1)	#Issue reset command
-raw_input('Hit enter to get started')
 
 while True:
 	Hex_command = raw_input("Input Command or bye to exit now")
 	if Hex_command == "bye" : break
-	print "Hex command is" Hex_command
+	print "Hex command is" (Hex_command)
 	print int(Hex_command, 16)
 	rs.send_code(Hex_command)
 	print "Command sent"
