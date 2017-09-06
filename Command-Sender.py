3f729edb90ef6c826a8398e5561857594124af72
Alex <alexcousins78@gmail.com>
2017-09-06 21:54:13 +0100

    test

--- Command-Sender.py
+++ Command-Sender.py
@@ -11,6 +11,7 @@ raw_input('Hit enter to get started')
 while True:
 	Hex_command = raw_input("Input Command or bye to exit now")
 	if Hex_command == "bye" : break
+	print Hex_command
 	print int(Hex_command, 16)
 	rs.send_code(Hex_command)
 	print "Command sent"
